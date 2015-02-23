dcd32518b9f051405e6efcd51b91b85711fc9b49
Juanpi <ferrarijuanp@gmail.com>
2015-02-23 17:16:41 -0300

    routes, controllers, services & template de AngularJS

--- public/main.js
+++ public/main.js
@@ -1,44 +1,15 @@
-var app = angular.module('mrmValidate', ['angularFileUpload']);
+'use_strict';
 
-app.controller('mainController',
-	function mainController($scope, $http, $upload) {
-
-		$scope.$watch('files', function() {
-			$scope.upload($scope.files);
-		});
-
-		$scope.upload = function(files) {
-			if (files && files.length) {
-				for (var i = 0; i < files.length; i++) {
-					var file = files[i];
-					$upload.upload({
-						url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-						fields: {
-							'username': $scope.username
-						},
-						file: file
-					}).progress(function(evt) {
-						var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-						console.log('progress: ' + progressPercentage + '% ' +
-							evt.config.file.name);
-					}).success(function(data, status, headers, config) {
-						console.log('file ' + config.file.name + 'uploaded. Response: ' +
-							JSON.stringify(data));
-					});
-				}
-			}
-		};
+var mrmApp = angular.module('mrmValidate', ['ngRoute', 'ngSanitize', 'angularFileUpload', 'validatorController']);
 
 
-
-		/*$http.get('/app/cssValidator').
-		success(function(data, status, headers, config) {
-			$scope.info = data;
-			//console.log(data);
-		}).
-		error(function(data, status, headers, config) {
-			console.log("Error " + data);
-		});*/
-
-
+mrmApp.config(['$routeProvider', function($routeProvider, $locationProvider) {
+	$routeProvider.
+	when('/validate', {
+		templateUrl: 'views/htmlValidator.html',
+		controller: 'htmlValidatorControler'
+	}).
+	otherwise({
+		redirectTo: '/validate'
 	});
+}]);
