cb28b7d1ff03c405164c622c2427d5aa661a3989
Juanpi <ferrarijuanp@gmail.com>
2015-03-09 16:34:02 -0300

    validador html front-end

--- public/app/controllers/validatorController.js
+++ public/app/controllers/validatorController.js
@@ -1,26 +1,35 @@
-'use_strict';
+'use strict';
 
 angular.module('validatorController', ['validatorService'])
 
 .controller({
     'HtmlValidatorControler': function($scope, $http, $upload, ValidatorService) {
 
-        $scope.submitFile = function(){
-            $scope.isDisabledButton = true;
+
+        $scope.submitFile = function(form) {
+
+            if (form.$valid) {
+                var fileUpload = {
+                    "files": $scope.files,
+                    "checkResponsibe": $scope.checkResponsibe || 'NO'
+                }
+                console.log("object", fileUpload);
+            }
 
         };
-        
+
+
         $scope.$watch('files', function() {
-            $scope.upload($scope.files);            
+            //$scope.upload($scope.files);
         });
 
-        $scope.upload = function(files) {
-            
+        /*$scope.upload = function(files) {
+
             if (files && files.length) {
-                
+
                 for (var i = 0; i < files.length; i++) {
                     var file = files[i];
-                   
+
                     $upload.upload({
                         url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
                         fields: {
@@ -31,15 +40,15 @@ angular.module('validatorController', ['validatorService'])
                         var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
                         console.log('progress: ' + progressPercentage + '% ' +
                             evt.config.file.name);
-                    }).success(function(data) { 
-                        //$scope.data = "data"; 
-                        console.log(data);                      
-                        /*console.log('file ' + config.file.name + 'uploaded. Response: ' +
-                            JSON.stringify(data));*/
+                    }).success(function(data) {
+
+
+                        console.log('file ' + config.file.name + 'uploaded. Response: ' +
+                            JSON.stringify(data));
                     });
                 }
             }
-        };
+        };*/
 
 
 
