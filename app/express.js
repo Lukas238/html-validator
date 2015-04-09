dad3f7504990a901c43f1e7e9ad1438a5b749136
Juanpi <ferrarijuanp@gmail.com>
2015-04-09 13:34:01 -0300

    handlers error express

--- app/express.js
+++ app/express.js
@@ -56,5 +56,34 @@ var app = express();
 // ROUTES
 app.use('/', router);
 
+// catch 404 and forward to error handler
+app.use(function(req,res,next){
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+//error handlers
+
+//development error handlers
+if(app.get('env') === 'development'){
+    app.use(function (err, req, res, next){
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+//production error handler
+/*app.use(function (err, req, res, next){
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});*/
+
 
 module.exports = app;
